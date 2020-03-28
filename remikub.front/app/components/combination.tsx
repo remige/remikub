2be78d6fa8b7312544892d540972b69778a94cac
rgrange <rgrange@vente-privee.com>
2020-03-28 18:02:47 +0100

    Disable move on hand

--- remikub.front/app/components/combination.tsx
+++ remikub.front/app/components/combination.tsx
@@ -19,18 +19,19 @@ export class Combination extends React.Component<ICombinationProps> {
     public render() {
         return <div style={{ display: "flex" }}>
             {this.props.combination.map((card, rank) => <div key={rank} style={{ display: "flex" }}>
-                {rank === 0 &&
+                {rank === 0 && this.props.place === "board" &&
                     <Card moveCard={fromCoorinates => this.props.store.moveCard(fromCoorinates, {
                         combinationId: this.props.combinationId,
                         rank,
                         place: this.props.place,
                     })} />}
                 <Card card={card} coordinates={{ combinationId: this.props.combinationId, rank, place: this.props.place }} />
-                <Card moveCard={fromCoorinates => this.props.store.moveCard(fromCoorinates, {
-                    combinationId: this.props.combinationId,
-                    rank: rank + 1,
-                    place: this.props.place,
-                })} />
+                {this.props.place === "board" &&
+                    <Card moveCard={fromCoorinates => this.props.store.moveCard(fromCoorinates, {
+                        combinationId: this.props.combinationId,
+                        rank: rank + 1,
+                        place: this.props.place,
+                    })} />}
             </div>)}
         </div>;
     }
